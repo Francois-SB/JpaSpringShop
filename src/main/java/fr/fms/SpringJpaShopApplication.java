5c914c7dd7d1f7b49bcfb3e50b1d2a0712fab29e
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-05-19 15:15:43 +0200

    Oui

--- src/main/java/fr/fms/SpringJpaShopApplication.java
+++ src/main/java/fr/fms/SpringJpaShopApplication.java
@@ -1,12 +1,14 @@
 package fr.fms;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.fms.business.Business;
 import fr.fms.business.BusinessImpl;
 import fr.fms.dao.ArticleRepository;
 import fr.fms.dao.CategoryRepository;
@@ -16,6 +18,12 @@ import fr.fms.entities.Category;
 @SpringBootApplication
 public class SpringJpaShopApplication implements CommandLineRunner {
 	public static final String TEXT_BLUE = "\u001B[36m";
+	public static final String TEXT_RESET = "\u001B[0m";	
+	private static final String COLUMN_ID = "IDENTIFIANT";
+	private static final String COLUMN_DESCRIPTION = "DESCRIPTION";
+	private static final String COLUMN_BRAND = "MARQUE";
+	private static final String COLUMN_PRICE = "PRIX";
+	private static final String COLUMN_NAME = "NAME";
 //	@Autowired
 //	private CategoryRepository categoryRepository;
 //
@@ -26,6 +34,7 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 	private BusinessImpl business;// = new BusinessImpl()
 
 	private static Scanner scan = new Scanner(System.in); 
+	private static String scanTmp;
 //	private static BusinessImpl business = new BusinessImpl();
 	
 	public static void main(String[] args) {
@@ -34,7 +43,7 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 //		Scanner scan = new Scanner(System.in);
-		String scanTmp;
+		
 		//		BusinessImpl business = new BusinessImpl();
 		//		Category smartphone = categoryRepository.save(new Category("Smartphone"));
 		//		Category tablet = categoryRepository.save(new Category("Tablet"));
@@ -55,17 +64,19 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 		while(choice < 11) {
 			displayMenu();
 			scanTmp = scan.nextLine();
+			
 			choice = Integer.parseInt(scanTmp);
+			scan.nextLine();
 			switch(choice) {
 			case 1 : displayOneArticle();				
 			break;					
-			case 2 : System.out.println("addArticle"); //addArticle();
+			case 2 : addArticle(); //addArticle();
 			break;					
 			case 3 : System.out.println("removeArticle");//removeArticle();
 			break;					
 			case 4 : System.out.println("modifyArticle");//modifyArticle();
 			break;						
-			case 5 : System.out.println("displayArticlesByCategoryId");//displayArticlesByCategoryId;
+			case 5 : displayArticlesByCategoryId();//displayArticlesByCategoryId;
 			break;
 			case 6 : displayCategories();//displayCategories();
 			break;
@@ -82,6 +93,8 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 			default : System.out.println("veuillez saisir une valeur entre 1 et 8");
 			}
 		}
+		scan.close();
+		
 	}
 
 
@@ -101,8 +114,46 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 		System.out.println("11 : sortir de l'application");
 	}
 	private void displayOneArticle() {
-		// TODO Auto-generated method stub
+		System.out.println("displayOneArticle");
+		int id = scanInt();
+		System.out.println("INT ID ; "+id);
+		Article article = new Article();
+		try {
+			article = business.readOneArticle((long)id);
+			
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("Pas d'articles pour cet Id, veuillez réessayer");
+		}
+		System.out.println(article);
 		
+	}
+	private void displayArticlesByCategoryId() {
+		displayCategories();
+		System.out.println("saisissez l'id de la catégorie concerné");
+		int id = scanInt();
+		Category category = business.readOneCategory(id);
+		if(category != null) {
+			System.out.printf("              AFFICHAGE PAR CATEGORIE    %n");
+			System.out.printf("                     %-10s               %n",category.getName());
+			System.out.printf("------------------------------------------------------------%n");
+			System.out.printf("%-15s | %-15s | %-15s | %-15s %n",COLUMN_ID,COLUMN_DESCRIPTION,COLUMN_BRAND,COLUMN_PRICE);
+			System.out.printf("------------------------------------------------------------%n");
+			business.readArticlesByCatId(id).forEach( a -> System.out.printf("%-15s | %-15s | %-15s | %-15s%n",a.getId(),a.getDescription(),a.getBrand(), a.getPrice()));
+		}
+		else System.out.println("cette catégorie n'existe pas !");
+		
+	}
+	private int scanInt() {
+		try {//(Scanner scann = new Scanner(System.in))
+			System.out.println("scanInt");
+			scanTmp = scan.nextLine();
+			System.out.println("Integer.parseInt(scanTmp) : "+Integer.parseInt(scanTmp));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		scan.nextLine();
+		return Integer.parseInt(scanTmp);
 	}
 	private void displayArticles() {
 		for(Article article : business.readArticles()) {
@@ -117,5 +168,54 @@ public class SpringJpaShopApplication implements CommandLineRunner {
 		for(Category category : business.readCategories()) {
 			System.out.println(category);
 		}	
+		System.out.println();
+	}
+	/**
+	 * Méthode qui ajoute un article au panier
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		System.out.println("Description :");
+		String descString = scan.nextLine();
+		article.setDescription(descString);
+		
+		System.out.println("Marque :");
+		String brandString = scan.nextLine();
+		article.setBrand(brandString);
+		
+		System.out.println("Prix :");
+		double priceString = Double.parseDouble(scan.nextLine());
+		article.setPrice(priceString);
+		
+		for (Category category : business.readCategories()) {
+			System.out.println(category);
+		}
+		System.out.println("Category :");
+		int catInt = scanInt();
+		Category newcategory = business.readOneCategory((long)catInt);
+		article.setCategory(newcategory);
+		//save TODO test save ok ?
+		business.addOneArticle(article);
+			
+		
+	}
+	public void updateArticle() {
+		//select art
+		System.out.println("Selectionner l'id de l'article à modifier");
+		int id = scanInt();
+		Article article = business.readOneArticle((long)id);
+		if(article != null) {
+			//modify
+			System.out.println(article);
+			//choix attribut à moif TODO
+			//maj bdd
+			try {
+				business.updateOneArticle(article);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else System.out.println("l'article que vous souhaitez ajouter n'existe pas, pb de saisi id");
+
 	}
 }
